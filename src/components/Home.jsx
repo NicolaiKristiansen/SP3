6368e996477c13ec6c84dc6b8ac1e67550a092a4
NicolaiKristiansen <nicolaikristiansen223@gmail.com>
2026-01-05 11:24:07 +0100

    Coding done. Now to deploy automaticly

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import styles from "./Home.module.css"
+
+const Home = () => {
+    return (
+        <>
+        <div className={styles.home}>
+        <h1>Velkommen til min Exam/SP3 opgave</h1>
+
+        <p>I denne opgave har jeg taget SP2 API'en og lavet en frontend til den</p>
+        </div>
+        </>
+    )
+}
+
+export default Home
